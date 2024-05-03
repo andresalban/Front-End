06ff31747278e304b5bbe3e6ff31a801a85b2b1b
andresalban <andresalban@unicauca.edu.co>
2024-05-03 16:43:27 -0500

    aula 3 terminada

--- 3.JavaScript manipulando elementos en el DOM/3.Aplicando audio y estilos/2049-FokusCurso-proyecto-inicial/script.js
+++ 3.JavaScript manipulando elementos en el DOM/3.Aplicando audio y estilos/2049-FokusCurso-proyecto-inicial/script.js
@@ -2,31 +2,51 @@ const html = document.querySelector('html');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
-const banner =document.querySelector('.app__image');
+const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica')
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3')
+
+musica.loop = true;
+inputEnfoqueMusica.addEventListener('change',()=>{
+    if (musica.paused){
+        musica.play()
+    }else {
+        musica.pause()
+    }
+})
 
 botonEnfoque.addEventListener('click', () => {
-    cambiarContexto('enfoque')
+    cambiarContexto('enfoque');
+    botonEnfoque.classList.add('active');
 });
 
 botonCorto.addEventListener('click', () => {
-    cambiarContexto('descanso-corto')
+    cambiarContexto('descanso-corto');
+    botonCorto.classList.add('active');
 });
 
 botonLargo.addEventListener('click', () => {
-   cambiarContexto('descanso-largo')
+    cambiarContexto('descanso-largo');
+    botonLargo.classList.add('active');
 });
 
-function cambiarContexto(contexto){
-    html.setAttribute('data-contexto',contexto);
-    banner.setAttribute('src',`./imagenes/${contexto}.png`);
-    switch(contexto){
+function cambiarContexto(contexto) {
+
+    botones.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
+
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `./imagenes/${contexto}.png`);
+    switch (contexto) {
         case 'enfoque':
             titulo.innerHTML = `Optimiza tu productividad,<br>
                 <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
             break;
-            case 'descanso-corto':
-                titulo.innerHTML = `¿Que tal tomar un respiro?<br>
+        case 'descanso-corto':
+            titulo.innerHTML = `¿Que tal tomar un respiro?<br>
                 <strong class="app__title-strong">¡Haz una pausa corta!.</strong> `
             break
         case 'descanso-largo':
