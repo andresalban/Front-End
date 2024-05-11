f1e00914317dfcbf0726581675ebb6090b3385b8
andresalban <andresalban@unicauca.edu.co>
2024-05-10 23:51:45 -0500

    aula 3 terminada

--- 6.JavaScript creando solicitudes/3.Creando nuevos elementos/JavaScript-requests-stage-1/js/conexionAPI.js
+++ 6.JavaScript creando solicitudes/3.Creando nuevos elementos/JavaScript-requests-stage-1/js/conexionAPI.js
@@ -4,8 +4,28 @@ async function listarVideos() {
     const conexionConvertida= conexion.json()
     //console.log(conexionConvertida)
     return conexionConvertida
+};
+
+async function enviarVideo(titulo,descripcion,url,imagem){
+    const conexion=  await fetch("http://localhost:3001/videos",{
+        method: "POST",
+        headers:{"content-type": "application/json"},
+        body:JSON.stringify({
+            titulo:titulo,
+            descripcion:`${descripcion} mil visualizaciones `,
+            url:url,
+            imagem:imagem,
+
+        })
+
+    })
+    const conexionConvertida= conexion.json()
+    return conexionConvertida;
 }
+
+
+
 export const conexionAPI = {
-    listarVideos
+    listarVideos,enviarVideo
 }
 //listarVideos()
