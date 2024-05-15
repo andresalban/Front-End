250f9fe401420178051202a6f3b309d685a4cf5f
andresalban <andresalban@unicauca.edu.co>
2024-05-15 00:41:41 -0500

    inicio aula5

---
+++ 6.JavaScript creando solicitudes/5.Tratando Errores/JavaScript-requests-stage-1/js/crearVIdeo.js
@@ -0,0 +1,19 @@
+import { conectaAPI } from "./conectaAPI.js";
+
+const formulario = document.querySelector("[data-formulario]");
+
+//validaciones
+
+async function crearVideo(evento){
+    evento.preventDefault();
+    const imagen= document.querySelector("[data-imagen]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo=document.querySelector("[data-titulo]").value;
+    const descripcion = Math.floor(Math.random()*10).toString();
+
+    await conectaAPI.crearVideo(titulo,descripcion,url,imagen)
+
+    window.location.href="../pages/envio-concluido.html"
+}
+
+formulario,addEventListener("submit",evento=>crearVideo(evento));
