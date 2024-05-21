d8b8794956dd11982e47d5b63f5affc684a7fe0e
andresalban <andresalban@unicauca.edu.co>
2024-05-20 21:23:41 -0500

    aula 3 terminada

--- 7.JavaScript validando formularios/3.Desarrollando Validaciones/alura-bank-main/js/main.js
+++ 7.JavaScript validando formularios/3.Desarrollando Validaciones/alura-bank-main/js/main.js
@@ -1,4 +1,5 @@
 import esUnCuil from "./validar-cuil.js";
+import esMayorDeEdad from "./validar-edad.js";
 
 const campoDeFormulario = document.querySelectorAll("[required]");
 
@@ -9,5 +10,8 @@ campoDeFormulario.forEach((campo) => {
 function verificarCampo(campo) {
     if (campo.name === "cuil" && campo.value.length >= 11) {
         esUnCuil(campo)
+    }
+    if (campo.name==="fecha_nacimiento" && campo.value!="" ) {
+        esMayorDeEdad(campo)
     }
 }
--- 7.JavaScript validando formularios/3.Desarrollando Validaciones/alura-bank-main/js/validar-cuil.js
+++ 7.JavaScript validando formularios/3.Desarrollando Validaciones/alura-bank-main/js/validar-cuil.js
@@ -1,12 +1,17 @@
-export default function esUnCuil(campo){
-    const cuil =campo.value.replace(/[-\/]/g,"");
-    tieneNumerosRepetidos(cuil)
-    console.log(cuil);
-    console.log(tieneNumerosRepetidos(cuil));
-}
+export default function esUnCuil(campo) {
+    const cuil = campo.value.replace(/[-\/]/g, "");
+    
+    if (tieneNumerosRepetidos(cuil)) {
+        console.log("valores Repetidos");
+    }else if (validarPrimeroDigitos(cuil) && validarDigitoVerificador(cuil)) {
+        console.log("Cuil valido");
+    } else {
+        console.log("cuil no existe");
+    }
+};
 
 function tieneNumerosRepetidos(cuil) {
-    const numerosRepetidos =[
+    const numerosRepetidos = [
         "00000000000",
         "11111111111",
         "22222222222",
@@ -20,5 +25,31 @@ function tieneNumerosRepetidos(cuil) {
 
     ];
     return numerosRepetidos.includes(cuil)
-    
+
+}
+
+function validarPrimeroDigitos(cuil) {
+    let primerosDigitos = cuil.substr(0, 2)
+    let digitosValidos = ['20', '23', '24', '27', '30', '33', '34']
+    return digitosValidos.includes(primerosDigitos)
+}
+
+function validarDigitoVerificador(cuil) {
+    let acumulado = 0
+    let factores = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+
+    for (let index = 0; index < 10; index++) {
+        acumulado += parseInt(cuil[index], 10) * factores[index]
+    }
+
+    let validadorTeorico = 11 - (acumulado % 11)
+    if (validadorTeorico == 11) {
+        validadorTeorico = 0
+    } else if (validadorTeorico == 10) {
+        validadorTeorico = 9
+    }
+
+    const digitoVerificador = parseInt(cuil[10], 10)
+    return digitoVerificador === validadorTeorico;
+
 }
