760a1933e36840ceecbc575f2abf62aad7a8edc7
andresalban <andresalban@unicauca.edu.co>
2024-05-20 23:43:11 -0500

    inicio aula 5

---
+++ 7.JavaScript validando formularios/5.Capturando imagen/alura-bank-main/js/validar-cuil.js
@@ -0,0 +1,57 @@
+export default function esUnCuil(campo) {
+    const cuil = campo.value.replace(/[-\/]/g, "");
+    
+    if (tieneNumerosRepetidos(cuil)) {
+        //console.log("valores Repetidos");
+        campo.setCustomValidity("Valores Repetidos")
+    }else if (validarPrimeroDigitos(cuil) && validarDigitoVerificador(cuil)) {
+        console.log("Cuil valido");
+    } else {
+      //  console.log("cuil no existe");
+        campo.setCustomValidity("Cuil No Existe")
+    }
+};
+
+function tieneNumerosRepetidos(cuil) {
+    const numerosRepetidos = [
+        "00000000000",
+        "11111111111",
+        "22222222222",
+        "33333333333",
+        "44444444444",
+        "55555555555",
+        "66666666666",
+        "77777777777",
+        "88888888888",
+        "99999999999"
+
+    ];
+    return numerosRepetidos.includes(cuil)
+
+}
+
+function validarPrimeroDigitos(cuil) {
+    let primerosDigitos = cuil.substr(0, 2)
+    let digitosValidos = ['20', '23', '24', '27', '30', '33', '34']
+    return digitosValidos.includes(primerosDigitos)
+}
+
+function validarDigitoVerificador(cuil) {
+    let acumulado = 0
+    let factores = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+
+    for (let index = 0; index < 10; index++) {
+        acumulado += parseInt(cuil[index], 10) * factores[index]
+    }
+
+    let validadorTeorico = 11 - (acumulado % 11)
+    if (validadorTeorico == 11) {
+        validadorTeorico = 0
+    } else if (validadorTeorico == 10) {
+        validadorTeorico = 9
+    }
+
+    const digitoVerificador = parseInt(cuil[10], 10)
+    return digitoVerificador === validadorTeorico;
+
+}
